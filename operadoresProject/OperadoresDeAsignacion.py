6bad30c20fb37b9851b05dc040faace16af7143e
Andres Rodriguez <andresr.sistemas@gmail.com>
2024-09-26 20:02:02 -0500

    Se crea el archivo OperadoresDeAsignacion

---
+++ operadoresProject/OperadoresDeAsignacion.py
@@ -0,0 +1,20 @@
+# Ejemplo con operadores de Asignación
+var1 = 5
+var2 = 12
+var3 = 8
+var4 = 14
+var5 = 6
+var6 = 8
+#asignamos variables
+var1 +=3 #var1 le estoy asignando var1+3 --Asignacion con suma
+var2 -= 3
+var3 *= 3
+var4 /= 3
+var5 %= 3
+var6 //= 3
+print(f"Asignación con suma: {var1}")
+print(f"Asignación con resta: {var2}")
+print(f"Asignación con multiplicación: {var3}")
+print(f"Asignación con división: {var4}")
+print(f"Asignación con residuo: {var5}")
+print(f"Asignación con division entera: {var6}")

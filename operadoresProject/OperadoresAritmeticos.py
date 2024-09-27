17d4fa0ade8230cf2d2304dd58a7b89232475c02
Andres Rodriguez <andresr.sistemas@gmail.com>
2024-09-26 19:46:26 -0500

    Se crea el proyec con el Archv OperadoresAritmeticos

---
+++ operadoresProject/OperadoresAritmeticos.py
@@ -0,0 +1,10 @@
+# Elemplo operadores Aritmeticos
+num1=int(input("Ingrese el número 1: "))
+num2=int(input("Ingrese el número 2: "))
+
+print(f"La suma de {num1} y {num2} es: {num1+num2}")
+print(f"La resta de {num1} y {num2} es: {num1-num2}")
+print(f"La division de {num1} y {num2} es: {num1/num2}")
+print(f"La multiplicación de {num1} y {num2} es: {num1*num2}")
+print(f"El modulo de {num1} y {num2} es: {num1%num2}")
+print(f"La division de {num1} y {num2} es: {num1//num2}")

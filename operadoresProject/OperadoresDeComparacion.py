bdffe72db4f7508f48f94eee989c02e5b09b6fcd
Andres Rodriguez <andresr.sistemas@gmail.com>
2024-09-26 20:54:37 -0500

    Creamos el archivo OperadoresdeCoparacion

---
+++ operadoresProject/OperadoresDeComparacion.py
@@ -0,0 +1,10 @@
+# EJm De comparación
+
+m = input("Ingrese el Dato 1: ")
+n = input("Ingrese el Dato 2: ")
+print(m==n)#Igualdad
+print(m>n)#Mayor que >
+print(m<n)#Menor que <
+print(m>=n)#Mayor o igual >=
+print(m<=n)#Menor o igual <=
+print(m!=n)#No igual ! o diferente
